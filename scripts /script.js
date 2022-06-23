ce1b94366724a4931cbe70ce781a5bceb343483c
Anthony Malary <amalary@bu.edu>
2022-06-23 14:34:17 -0700

    Added name changing functionality to music player

--- scripts /script.js
+++ scripts /script.js
@@ -3,10 +3,17 @@ musicImg = container.querySelector(".img-area img"),
 musicName = container.querySelector(".song-details .name"),
 musicArtist = container.querySelector(".song-details .artist");
 
-let musicIndex = 1;
+let musicIndex = 3;
 
 window.addEventListener("load", ()=> {
 
     loadMusic(musicIndex)
+});
 
-})
+// function to load music 
+
+function loadMusic(indexNumb){
+
+    musicName.innerText = allMusic[indexNumb - 1].name
+
+}
