945d3b9bc3b92a79852af38211a9392021a7b654
Anthony Malary <amalary@bu.edu>
2022-06-24 13:20:05 -0700

    Play music button functionality

--- scripts /script.js
+++ scripts /script.js
@@ -2,8 +2,10 @@ const container = document.querySelector(".container"),
 musicImg = container.querySelector(".img-area img"),
 musicName = container.querySelector(".song-details .name"),
 musicArtist = container.querySelector(".song-details .artist");
+musicAudio = container.querySelector("#main-audio"),
+playPauseBtn = container.querySelector(".play-pause");
 
-let musicIndex = 5;
+let musicIndex = 2;
 
 window.addEventListener("load", ()=> {
 
@@ -17,7 +19,16 @@ function loadMusic(indexNumb){
     musicName.innerText = allMusic[indexNumb - 1].name
     musicArtist.innerText = allMusic[indexNumb - 1].artist
     musicImg.src = `images/${allMusic[indexNumb - 1].img}`
+    musicAudio.src = `songs/${allMusic[indexNumb - 1].src}`
 
 }
 
+// Play or music button event 
+
+playPauseBtn.addEventListener("click", ()=>{
+
+    const isMusicPaused = containter.containts("paused");
+
+    isMusicPaused ? pauseMusic() : playMusic(); 
+})
 
