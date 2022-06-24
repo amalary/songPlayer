41b03190c601f10ca0169667f224ffbaa06e9bac
Anthony Malary <amalary@bu.edu>
2022-06-24 13:40:24 -0700

    Adding other button functionality

--- scripts /script.js
+++ scripts /script.js
@@ -4,6 +4,8 @@ musicName = container.querySelector(".song-details .name"),
 musicArtist = container.querySelector(".song-details .artist");
 musicAudio = container.querySelector("#main-audio"),
 playPauseBtn = container.querySelector(".play-pause");
+nextBtn = container.querySelector("#next");
+prevBtn = container.querySelector("#prev");
 
 let musicIndex = 2;
 
@@ -19,16 +21,40 @@ function loadMusic(indexNumb){
     musicName.innerText = allMusic[indexNumb - 1].name
     musicArtist.innerText = allMusic[indexNumb - 1].artist
     musicImg.src = `images/${allMusic[indexNumb - 1].img}`
-    musicAudio.src = `songs/${allMusic[indexNumb - 1].src}`
+    mainAudio.src = `songs/${allMusic[indexNumb - 1].src}`
 
+}
+
+function playMusic(){
+    container.classList.add("paused")
+    playPauseBtn.querySelector("i").innerText = "pause"
+    mainAudio.play()
+}
+
+function pauseMusic(){
+    container.classList.add("paused")
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause()
 }
 
 // Play or music button event 
 
 playPauseBtn.addEventListener("click", ()=>{
 
-    const isMusicPaused = containter.containts("paused");
+    const isMusicPaused = container.classList.contains("paused");
 
     isMusicPaused ? pauseMusic() : playMusic(); 
-})
+});
+
+
+// Next button event 
+
+playPauseBtn = container.querySelector(".play-pause");
+
+
+
+
+
+
+
 
