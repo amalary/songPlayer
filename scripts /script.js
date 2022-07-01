5ff0ae9784169557c6da763e18b0dafc88502f44
Anthony Malary <amalary@bu.edu>
2022-07-01 13:05:25 -0700

    Creating the load music function

--- scripts /script.js
+++ scripts /script.js
@@ -3,6 +3,21 @@
 const container = document.querySelector(".container"),
 musicImg = container.querySelector(".img-area img"),
 musicName = container.querySelector(".song-details .name"),
-musicArtist = container.querySelector(".song-details .artist"),
+musicArtist = container.querySelector(".song-details .artist"); 
 
+
+let musicIndex = 5; 
+
+window.addEventListener("load", ()=> {
+    loadMusic(musicIndex)
+}); 
+
+// Function to load the music 
+
+function loadMusic(IndexNum){
+
+    musicName.innerText = allMusic[IndexNum - 1].name
+    musicArtist.innerText = allMusic[IndexNum - 1].artist
+
+}
 
