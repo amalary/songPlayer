90f04930977c76a862d30355991ad0061c304908
Anthony Malary <amalary@bu.edu>
2022-07-01 12:55:09 -0700

    redoing the logic

--- scripts /music-list.js
+++ scripts /music-list.js
@@ -1,45 +1,40 @@
 let allMusic = [
 
-
-{
-    name:"Too Much",
-    artist:"Drake",
-    img:"drakeCoverArt.jpg",
-    src:"Too Much.mp3",
-
-},
-
-{
-    name:"Just Made Bail",
-    artist:"Bas",
-    img:"bas.jpg",
-    src:"Made Bail.mp3",
-
-},
-
-{
-    name:"Shoutout",
-    artist:"Cousin Stizz",
-    img:"cousinStizz.jpg",
-    src:"Shoutout.mp3",
-
-},
-
-{
-    name:"Alright",
-    artist:"Kendrick Lamar",
-    img:"kendrickCover.jpg",
-    src:"Alright.mp3",
-
-},
-
-{
-    name:"Runaway",
-    artist:"Kanye West",
-    img:"kanyeCover.jpg",
-    src:"Runaway.mp3",
-
-}
-
+    {
+        name:"Too Much",
+        artist:"Drake",
+        img:"./images/drakeCoverArt.jpg",
+        src: "./songs/Intro",
+    },
+    {
+        name:"Made Bail",
+        artist:"Bas",
+        img:"./images/bas.jpg",
+        src: "./songs/All Alone",
+    },
+    {
+        name:"Shoutout",
+        artist:"Cousin Stizz",
+        img:"./images/cousinStizz.jpg",
+        src: "./songs/Love Me",
+    },
+    {
+        name:"Alright",
+        artist:"Kendrick Lamar",
+        img:"./images/kendrickCover.jpg",
+        src: "./songs/Late Night",
+    },
+    {
+        name:"Runaway",
+        artist:"Kanye West",
+        img:"./images/kanyeCover.jpg",
+        src: "./songs/Will",
+    },
+    {
+        name:"KOD",
+        artist:"J.Cole",
+        img:"./images/kodCoverArt.jpg",
+        src: "./songs/Special",
+    },
 
 ]
